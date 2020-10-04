5f90fa7ee6ee7d08c705f10e1ee1c777f20f7253
Yagizcan Seheri <yagizcanseheri@hotmail.com>
2020-10-04 12:52:12 +0300

    Category Controller

---
+++ SportsNews.UI/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,62 @@
+﻿using SportsNews.Entities;
+using SportsNews.Service.Services;
+using SportsNews.UI.Areas.Admin.Data.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsNews.UI.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryService _categoryService;
+        public CategoryController()
+        {
+            _categoryService = new CategoryService();
+        }
+        // GET: Admin/Category
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Category data) 
+        {
+            _categoryService.Add(data);
+            return Redirect("/Admin/Category/List");
+        }
+        public ActionResult List() 
+        {
+            List<Category> model = _categoryService.GetActive();
+            return View(model);
+        }
+
+        public ActionResult Update(int id) 
+        {
+            Category category = _categoryService.GetById(id);
+            CategoryDTO model = new CategoryDTO();
+            model.Id = category.Id;
+            model.Name = category.Name;
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Update(CategoryDTO model) 
+        {
+            Category category = _categoryService.GetById(model.Id);
+            category.Name = model.Name;
+            category.UpdateDate = DateTime.Now;
+            category.Status = Status.Modified;
+            _categoryService.Update(category);
+            return Redirect("/Admin/Category/List");
+        }
+        public ActionResult Delete(int id) 
+        {
+            _categoryService.Remove(id);
+            return Redirect("/Admin/Category/List");
+        }
+    }
+}
---
+++ SportsNews.UI/Areas/Admin/Data/DTO/CategoryDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SportsNews.UI.Areas.Admin.Data.DTO
+{
+    public class CategoryDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
